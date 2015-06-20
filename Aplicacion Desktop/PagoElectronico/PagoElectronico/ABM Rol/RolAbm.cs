3d495531ce35389201c9fc6340c581dd0c4be29e
Tinchox <martin.d.mza@gmail.com>
2015-06-20 18:28:13 -0300

    Rol Fix

--- Aplicacion Desktop/PagoElectronico/PagoElectronico/ABM Rol/RolAbm.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/ABM Rol/RolAbm.cs
@@ -255,7 +255,7 @@ namespace ABM
         //-----------------------------------------------------------------------------------------------------------------
 
         //-----------------------------------------------------------------------------------------------------------------
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void buttonBuscar_Click(object sender, EventArgs e)
         {
             fillRolesTable();
         }
